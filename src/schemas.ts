bd99b52db9dbb53086b6d17b7d1ffb0a627ee5d3
Hubizon <hubert@jastrzebscy.org>
2025-02-22 22:02:08 +0100

    Added new schemas

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,11 +1,13 @@
 import { z } from 'zod';
 
-// TODO: Choose types
 export const SlideAssessment = z.enum([
   'opinion',
   'verified-fact',
   'unverified-fact',
   'false-information',
+  'misleading',
+  'satire',
+  'unclassified'
 ]);
 
 export const Link = z.object({
@@ -13,14 +15,25 @@ export const Link = z.object({
   title: z.string(),
 });
 
+export const SlideAnalysis = z.object({
+  text_part: z.string(),
+  category: SlideAssessment,
+  explanation: z.string().max(40),
+  word_indexes: z.tuple([z.number(), z.number()]),
+  links: Link,
+  suggestions: z.array(z.string()),
+});
+export type SlideAnalysis = z.infer<typeof SlideAnalysis>;
+
 export const Slide = z.object({
   timestampBeginMs: z.number().positive(),
   durationMs: z.number().positive(),
-  // transcript: z.string(),
   comment: z.optional(z.string()),
   assessment: SlideAssessment,
   links: z.array(Link),
+  suggestions: z.array(z.string()),
 });
+export type Slide = z.infer<typeof Slide>;
 
 export const Movie = z.object({
   slides: z.array(Slide),
