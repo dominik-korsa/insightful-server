260b0c472eba6fb9cf09107fd678c2d9547595f4
dominik-korsa <29484605+dominik-korsa@users.noreply.github.com>
2025-02-22 16:15:47 +0100

    Add sample schema

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,12 +1,21 @@
 import { z } from 'zod';
 
-/// Schema for a sentence or part of a sentence transcription
-export const SentenceSchema = z.object({
-  /// The text of the sentence
-  text: z.string(),
-  /// The start time of the sentence in seconds
-  startTime: z.number(),
-  /// The end time of the sentence in seconds
-  endTime: z.number(),
+// TODO: Choose types
+export const SlideAssessment = z.enum([
+  'opinion',
+  'verified-fact',
+  'unverified-fact',
+  'rhetorical-question',
+]);
+
+export const Slide = z.object({
+  timestampBeginMs: z.number().positive(),
+  durationMs: z.number().positive(),
+  transcript: z.string(),
+  comment: z.string(),
+  assessment: SlideAssessment,
 });
-  
+
+export const Movie = z.object({
+  slides: z.array(Slide),
+});
