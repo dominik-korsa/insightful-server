5bdeadb2283a1e6ae424ba761a5fe56bb72219d4
Hubizon <hubert@jastrzebscy.org>
2025-02-23 03:37:35 +0100

    Added word indices to the prompt

--- src/bin/debug.ts
+++ src/bin/debug.ts
@@ -1,14 +1,14 @@
 import 'dotenv/config';
-import { downloadMp4, mp4toMp3, transcribe, processWhisperResult } from "../video.js";
+import {processWhisperResult} from "../video.js";
 
 import fs from 'fs';
 import path from 'path';
-import { WhisperOutput } from '@fal-ai/client/endpoints';
+import {WhisperOutput} from '@fal-ai/client/endpoints';
 
 async function main() {
   const transcript = JSON.parse(await fs.promises.readFile(path.join(import.meta.dirname, '../../local/transcript.json'), 'utf-8')) as WhisperOutput;
-
-  await processWhisperResult(transcript)
+  const movie = await processWhisperResult(transcript)
+  console.log(movie);
 }
 
 main().catch((error) => {
--- src/video.ts
+++ src/video.ts
@@ -1,7 +1,7 @@
 import {fal, Result} from "@fal-ai/client";
 import ffmpeg from 'fluent-ffmpeg';
 import {Readable, Writable} from 'stream';
-import {WhisperOutput} from '@fal-ai/client/endpoints';
+import {WhisperChunk, WhisperOutput} from '@fal-ai/client/endpoints';
 import {Movie, Slide, SlideAnalysis} from './schemas.js';
 import {z} from 'zod';
 import got from "got";
@@ -56,7 +56,8 @@ export async function downloadMp4(videoUrl: string): Promise<Buffer> {
   return got.get(videoUrl).buffer();
 }
 
-async function createPrompt(input_text: string): Promise<string> {
+async function createPrompt(chunks: WhisperChunk[]): Promise<string> {
+  const input_text = chunks.map((chunk, index) => `[${index}] ${chunk.text}`).join(" ")
   const promptTemplate = await fs.promises.readFile(
     path.join(import.meta.dirname, '../assets/prompt.txt'), 'utf-8');
   return `${promptTemplate}\n"${input_text}"\nOutput JSON:`;
@@ -84,14 +85,13 @@ async function analyzeSlide(prompt: string): Promise<SlideAnalysis[]> {
 }
 
 export async function processWhisperResult(whisperResult: WhisperOutput): Promise<Movie> {
-  const input_text = whisperResult.text
-  const prompt = await createPrompt(input_text);
-  const analysis = await analyzeSlide(prompt);
-
   const chunks = whisperResult.chunks;
   if (chunks === undefined) {
     throw new Error("No word timings found");
   }
+
+  const prompt = await createPrompt(chunks);
+  const analysis = await analyzeSlide(prompt);
 
   const getTimestamp = (item: SlideAnalysis, id: number): number => {
     return Number(chunks[item.word_indexes[id]].timestamp[id]);
@@ -107,5 +107,5 @@ export async function processWhisperResult(whisperResult: WhisperOutput): Promis
     transcript: item.text_part,
   }));
 
-  return { slides: slides };
+  return {slides: slides};
 }
